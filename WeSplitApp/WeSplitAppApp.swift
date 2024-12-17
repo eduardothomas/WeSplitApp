56c6f6b88cb788914eb081b7d2ed15816737a54a
Eduardo Thomas <eduardothomas64@gmail.com>
2024-12-16 19:55:42 -0500

    Initial Commit

---
+++ WeSplitApp/WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAppApp.swift
+//  WeSplitApp
+//
+//  Created by Eduardo Thomas on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
